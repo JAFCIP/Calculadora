b8e96f54d9c8e87b5c7fa496bce2eb63c7004bcd
JAFCIP <59651247+JAFCIP@users.noreply.github.com>
2021-03-21 16:05:02 +0100

    Add files via upload

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,187 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+
+ * Clase publica de la calculadora donde se implementan los JFrames, JTextField, JButton, JPanel y fuentes de texto.
+
+ * @author: Jose Antonio Blancas Rodriguez
+
+ * @version: 20/03/2021
+
+ */
+
+public class Calculadora implements ActionListener {
+
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButton = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
+    /**
+     * Constructor de Calculadora donde se define la ventana, los votones los textfields.
+
+     * @param textfield Define el panel de texto con la fuente de texto elegidas de forma propia donde apareceran los numeros seleccionados.
+
+     * @param functionButtons Define la funcion de los diferentes tipos de botones añadidos al programa de la calculadora.
+
+     * @param panel Define los paneles de los botones que apareceran en la interfaz de la calculadora visibles cuando se ejecute el programa.
+     */
+    Calculadora() {
+
+        frame = new JFrame("Calculadora");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50, 25, 300, 50);
+        textfield.setFont(myFont);
+        textfield.setEditable(false);
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        for(int i = 0; i < 8; i++){
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        for(int i = 0; i < 10; i++){
+            numberButton[i] = new JButton(String.valueOf(i));
+            numberButton[i].addActionListener(this);
+            numberButton[i].setFont(myFont);
+            numberButton[i].setFocusable(false);
+        }
+
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        panel.add(numberButton[1]);
+        panel.add(numberButton[2]);
+        panel.add(numberButton[3]);
+        panel.add(addButton);
+        panel.add(numberButton[4]);
+        panel.add(numberButton[5]);
+        panel.add(numberButton[6]);
+        panel.add(subButton);
+        panel.add(numberButton[7]);
+        panel.add(numberButton[8]);
+        panel.add(numberButton[9]);
+        panel.add(mulButton);
+        panel.add(decButton);
+        panel.add(numberButton[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
+        frame.add(textfield);
+        frame.setVisible(true);
+    }
+    
+    /**
+     * Clase para poder ejecutar el codigo.
+     */
+    
+    public static void main(String[] args){
+
+    	Calculadora calc = new Calculadora();
+    }
+
+    /**
+     * Clase donde se realizan las diferentes funciones de operaciones de la calculadora.
+     */
+
+    public void actionPerformed(ActionEvent e){
+
+        for(int i = 0; i <10; i++){
+            if(e.getSource() == numberButton[i]){
+                textfield.setText(textfield.getText().concat(String.valueOf(i)));
+            }
+        }
+        if(e.getSource() == decButton){
+            textfield.setText(textfield.getText().concat("."));
+        }
+        if(e.getSource() == addButton){
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '+';
+            textfield.setText("");
+        }
+        if(e.getSource() == subButton){
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '-';
+            textfield.setText("");
+        }
+        if(e.getSource() == mulButton){
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '*';
+            textfield.setText("");
+        }
+        if(e.getSource() == divButton){
+            num1 = Double.parseDouble(textfield.getText());
+            operator = '/';
+            textfield.setText("");
+        }
+        if(e.getSource() == equButton){
+            num2 = Double.parseDouble(textfield.getText());
+            
+            switch(operator){
+                case '+':
+                    result=num1+num2;
+                    break;
+                case '-':
+                    result=num1-num2;
+                    break;
+                case '*':
+                    result=num1*num2;
+                    break;
+                case '/':
+                    result=num1/num2;
+                    break;
+            }
+            textfield.setText(String.valueOf(result));
+            num1 = result;
+        }
+        if(e.getSource() == clrButton) {
+            textfield.setText("");
+        }
+        if(e.getSource() == delButton) {
+            String string = textfield.getText();
+            textfield.setText("");
+            for(int i = 0; i < string.length() - 1; i++) {
+                textfield.setText(textfield.getText() + string.charAt(i));
+            }
+        }
+    }
+}
---
+++ javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Calculator","l":"actionPerformed(ActionEvent)","url":"actionPerformed(java.awt.event.ActionEvent)"},{"p":"<Unnamed>","c":"Calculator","l":"main(String[])","url":"main(java.lang.String[])"}]
